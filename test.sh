a7067829d5cb591b15107ee83573d43eec92b832
nadutudor <nadu.tudor@gmail.com>
2025-12-09 01:58:43 +0200

    main structure, to be built upon

---
+++ test.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+declare -A children
+declare -A valori
+declare -a stiva
+
+add_child() {
+    local parent=$1
+    local child=$2
+    children[$parent]="${children[$parent]} $child"
+}
+
+add_valori() {
+	local parent="$1"
+	local valoare="$2"
+	valori["$parent"]="${valori["$parent"]} $valoare"
+}
+
+push_stiva() {
+	local item=$1
+	stiva+=( "$item" )
+}
+
+pop_stiva() {
+	if [ ${#stiva[@]} -gt 0 ]; then
+		unset stiva[-1]
+	fi
+}
+
+print_tree_tags() {
+    local node=$1
+    local indent=$2
+
+    echo "${indent}${node}"
+	if [[ ! -z "${valori[$node]//[[:space:]]/}" ]]; then
+		echo " $indent${valori[$node]}"
+	fi
+    for child in ${children[$node]}; do
+        print_tree_tags "$child" "  $indent"
+    done
+}
+
+push_stiva "root"
+
+# This marks the beginning of the program
+if [[ -z "$1" ]]; then
+	echo "Nu a fost introdus fisier"
+	exit 1
+fi
+
+if [[ ! -f "$1" ]]; then
+	echo "Fisierul dat nu a fost gasit"
+	exit 1
+fi
+
+val=""
+while IFS="" read -r linie; do
+	for ((i=0; i<${#linie}; i++)); do
+		char="${linie:i:1}"
+			if [[ "$char" == "<" ]]; then
+				if [ ${#stiva[@]} -gt 0 ]; then
+					val=$(echo "$val" | tr -s '[:space:]' ' ' | sed 's/^ *//;s/ *$//')
+					add_valori "${stiva[-1]}" "$val"
+					val=""
+				fi
+				if [[ "${linie:((i+1)):1}" == "/" ]]; then
+					while [[ "${linie:i:1}" != ">" ]]; do
+						((i++))
+					done
+					pop_stiva
+				else
+					tag_name=""
+					for ((j=i; j<${#linie}; j++)); do
+						if [[ "${linie:j:1}" == ">" ]]; then
+							break
+						fi
+						tag_name+="${linie:j:1}"
+					done
+					tag_name+=">"
+					i=$j
+					add_child "${stiva[-1]}" "$tag_name"
+					push_stiva "$tag_name"
+				fi
+			else
+				val+="$char"
+			fi
+	done
+done < "$1"
+print_tree_tags root ""
